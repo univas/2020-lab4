d92ba9f81fc6e3dc685b76c1f791eec5e7285882
Rodrigo Faria <rodrigolfsi@gmail.com>
2020-09-24 21:03:23 -0300

    Add example of change class of element dynamically

---
+++ exercicio-5/js/index.js
@@ -0,0 +1,54 @@
+let guestList = []
+
+start()
+
+function start() {
+  const button = document.getElementById('btnAdd')
+  button.onclick = function() {
+    addButtonEventListener()  
+  }
+}
+
+function addButtonEventListener() {
+  const input = document.getElementById('guest')
+  if (isEmpty(input.value)) {
+    showRequiredFieldMessage()
+
+  } else if (isNameInTheArray(input.value)) {
+    hideRequiredFieldMessage()
+    alert('Este convidado já está na lista!')
+  
+  } else {
+    hideRequiredFieldMessage()
+    addNewGuest(input.value)
+    clearAndFocus(input)
+  }
+}
+
+function isEmpty(guestName) {
+  return guestName.trim() === ''
+}
+
+function isNameInTheArray(guestName) {
+  return guestList.includes(guestName)
+}
+
+function addNewGuest(guestName) {
+  guestList.push(guestName)
+  console.log(guestList)
+}
+
+function clearAndFocus(input) {
+  input.value = ''
+  input.focus()
+}
+
+function showRequiredFieldMessage() {
+  const p = document.getElementById('requiredFieldMessage')
+  p.className = ''
+}
+
+function hideRequiredFieldMessage() {
+  const p = document.getElementById('requiredFieldMessage')
+  p.className = 'hidden'
+}
