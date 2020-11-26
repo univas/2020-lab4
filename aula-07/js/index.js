8b75bf4919a81734fae9c90160c3de2c15f81bab
Rodrigo Faria <rodrigolfsi@gmail.com>
2020-11-26 20:42:59 -0300

    Implements CRUD

--- aula-07/js/index.js
+++ aula-07/js/index.js
@@ -1,3 +1,5 @@
+let categoryId = null
+
 const start = () => {
   loadDataFromServer()
 
@@ -7,6 +9,8 @@ const start = () => {
 const loadDataFromServer = () => {
   $.get('https://financial-control-api-node.herokuapp.com/api/v1/categories', data => {
     populateTable(data)
+
+    setOnClickButtons()
   })
 }
 
@@ -19,10 +23,10 @@ const populateTable = data => {
     <tr>
       <td>${item.name}</td>
       <td>
-        <input type="button" value="Editar">
+        <input type="button" value="Editar" class="editButton" name="${item.id}">
       </td>
       <td>
-        <input type="button" value="Excluir">
+        <input type="button" value="Excluir" class="deleteButton" name="${item.id}">
       </td>
     </tr>`)
   })
@@ -32,11 +36,65 @@ const saveNewCategory = () => {
   const category = {
     name: $('#categoryName').val()
   }
-  
-  $.post('https://financial-control-api-node.herokuapp.com/api/v1/categories', category)
-    .done(() => loadDataFromServer())
 
+  if (categoryId === null) {
+    $.post('https://financial-control-api-node.herokuapp.com/api/v1/categories', category)
+      .done(() => loadDataFromServer())
+  } else {
+    edit(category)
+    categoryId = null
+  }
+  
   $('#categoryName').val(null) 
+}
+
+const setOnClickButtons = () => {
+  $('.deleteButton').click(deleteCategory)
+
+  $('.editButton').click(editCategory)
+}
+
+const deleteCategory = event => {
+  const deleteButton = event.target
+
+  $.ajax({
+    url: `https://financial-control-api-node.herokuapp.com/api/v1/categories/${deleteButton.name}`,
+    type: 'DELETE',
+    success: () => {
+      alert('Categoria excluida com sucesso!')
+      loadDataFromServer()
+    },
+    error: () => {
+      alert('Falha ao excluir a categoria!')
+    }
+  })
+}
+
+const editCategory = event => {
+  const editButton = event.target
+  const td = editButton.parentNode
+  const tr = td.parentNode
+  const columnsByLine = tr.getElementsByTagName('td')
+  const columnCategoryName = columnsByLine[0]
+
+  $('#categoryName').val(columnCategoryName.innerHTML)
+  
+  categoryId = editButton.name
+}
+
+const edit = category => {
+  $.ajax({
+    url: `https://financial-control-api-node.herokuapp.com/api/v1/categories/${categoryId}`,
+    type: 'PUT',
+    data: category,
+    success: () => {
+      alert('Categoria editada com sucesso!')
+      loadDataFromServer()
+    },
+    error: () => {
+      alert('Falha ao editar a categoria!')
+    }
+  })
 }
 
 start()
